69df15db250c608a3d74252088344d28433e803a
larimae <larissamae60@gmail.com>
2024-09-25 09:11:05 -0700

    added renderLicenseSection

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -16,6 +16,10 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (!license) return '';
+  return `
+##License
+This project is licensed under the ${license} license.`;
 
 }
 
